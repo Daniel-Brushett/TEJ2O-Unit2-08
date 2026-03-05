7d87cc31e10f656dedcd01944b4f41c87d5a6996
Daniel-Brushett <dj.brushett@stu.ocsb.ca>
2026-03-05 13:26:09 -0500

    added code and changed information

--- main.ts
+++ main.ts
@@ -1,8 +1,114 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
+ * Created by: Daniel .J Brushett
  * Created on: Sep 2020
- * This program ...
+ * This program changes colors on LED
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+
+    // Turn LED "Red" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Red")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+ 
+    // Turn LED "Green" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Green")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // Turn LED "Blue" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Blue")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // Turn LED "Magenta" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Magenta")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // Turn LED "Cyan" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Cyan")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // Turn LED "Yellow" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("Yellow")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // Turn LED "White" for 1 s
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.showString("White")
+
+    // wait 1 s
+    basic.pause(1000)
+
+    // turn off LED
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // final action
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
